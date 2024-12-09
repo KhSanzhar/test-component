42d5792245ff38daefcf30b2b5a84803f5e7194c
KhSanzhar <sanzhkhalel@gmail.com>
2024-12-09 19:37:36 +0500

    feat: structure

---
+++ components/ui/side-bar/SideBar.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const links = [
+  {
+    label: 'Dashboard',
+    to: '/dashboard',
+  },
+  {
+    label: 'Create',
+    to: '/create',
+  }
+]
+</script>
+<template>
+  <div class="absolute top-0 left-0 bg-gray-200 h-full w-1/12 flex justify-center items-center">
+    <u-vertical-navigation :links="links" class="w-full"/>
+  </div>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import SideBar from "~/components/ui/side-bar/SideBar.vue";
+</script>
+<template>
+  <div>
+    <side-bar/>
+  </div>
+</template>
